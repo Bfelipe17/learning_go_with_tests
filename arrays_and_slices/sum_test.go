39b955a46b272b405df9b4a8755bb734001a1878
Bfelipe17 <bruno.felipe17@hotmail.com>
2024-02-09 23:35:10 -0300

    structs, methods & interfaces

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -19,7 +19,6 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-
 	t.Run("make the sums of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
@@ -28,7 +27,6 @@ func TestSumAllTails(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
-
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
@@ -37,5 +35,4 @@ func TestSumAllTails(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
-
 }
